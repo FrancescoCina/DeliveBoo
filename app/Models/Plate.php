7fe4c3c0037cf638084c8fe7cafa7c12148dec4b
Pasquale-Raso <padqualeraso@gmail.com>
2021-11-04 17:28:30 +0100

    Creata relazione 1 a molti tra ristoranti  epiatti

--- app/Models/Plate.php
+++ app/Models/Plate.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Plate extends Model
 {
-    //
+    public function restaurant()
+    {
+        return $this->belongsTo('App\Models\Restaurant');
+    }
 }
