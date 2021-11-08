e787843996506e9a8db0914a32afe15b7275f6a8
daniele-pipino <Danielemacpipino@icloud.com>
2021-11-08 19:55:51 +0100

    added value to the form inputs

--- resources/views/admin/plates/index.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.app')
-@section('content')
-
-  <a href="{{route('admin.plates.create')}}">ciao ciao</a>
-    
-@endsection
--- resources/views/includes/form.blade.php
+++ resources/views/includes/form.blade.php
@@ -8,24 +8,24 @@
         @method('PATCH')
     @else
        <h1 class="text-center">Crea Piatto</h1>
-       <form class="w-75 mx-auto" method="POST" action="{{route('admin.plates.store')}}">
+       <form class="w-75 mx-auto" method="POST" action="{{route('admin.plates.create')}}">
     @endif
         @csrf
        <div class="form-group">
          <label for="name">Nome del piatto</label>
-         <input type="text" class="form-control" id="name" name='name' placeholder="Inserisci il nome del piatto">
+         <input type="text" class="form-control" id="name" name='name' placeholder="Inserisci il nome del piatto">{{$plate->name}}
        </div>
        <div class="form-group">
           <label for="image">Immagine</label>
-          <input type="file" class="form-control" id="image" name='image'>
+          <input type="file" class="form-control" id="image" name='image'>{{$plate->image}}
         </div>
         <div class="form-group">
           <label for="Description">Descrizione</label>
-          <textarea class="form-control" id="Description" name="description" ></textarea>
+          <textarea class="form-control" id="Description" name="description" >{{$plate->description}}</textarea>
         </div>
         <div class="form-group">
           <label for="price">Prezzo</label>
-          <input type="text" class="form-control" id="price" name='price' placeholder="Inserisci il prezzo del piatto">
+          <input type="text" class="form-control" id="price" name='price' placeholder="Inserisci il prezzo del piatto">{{$plate->price}}
         </div>
         <div class="form-group">
           <label for="serving">Portata</label>
@@ -40,10 +40,16 @@
         @foreach ($categories as $category) 
           <div class="form-check form-check-inline">
              <input class="form-check-input" type="checkbox" id="{{$category->id}}" value="option1">
-             <label class="form-check-label" for="{{$category->id}}" name="categories[]" @if(in_array($category->id , old('category', $categoryId ?? []))) checked @endif>{{$category->name}}</label>
+             <label class="form-check-label" for="{{$category->id}}" name="categories[]" @if(in_array($category->id , old('category', $categoriesIds ?? []))) checked @endif>{{$category->name}}</label>
           </div>   
        @endforeach
-         
+       <div class="my-2 d-flex justify-content-between">
+         <button class='btn btn-success'type="submit">Salva</button>
+         <a class='btn btn-outline-secondary' href="{{route('admin.plates.index')}}">Indietro</a>
+       </div>
+       
+      
+
      </form>
    </section>   
 @endsection
