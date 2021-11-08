c6e0ad0cce74231ef180d2125d6bf1d86312d175
FrancescoCina <francesco.cina04@gmail.com>
2021-11-08 13:46:45 +0100

    Add route to Set crud

---
+++ app/Http/Controllers/Admin/RestaurantController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RestaurantController extends Controller
+{
+    public function index()
+    {
+        // $restaurants = $user->restaurant;
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,33 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
 
 
 // ROTTE DEL BACKEND
 
+Route::middleware('auth')->name('admin.')->prefix('admin')->namespace('Admin')->group(function () {
+    // restaurants routes
+    Route::get('/', 'RestaurantController@index')->name('restaurants.index');
+    Route::get('/restaurants/create', 'RestaurantController@create')->name('restaurants.create');
+    Route::post('/restaurants', 'RestaurantController@store')->name('restaurants.store');
+    // plates routes
+    Route::get('/plates', 'PlateController@index')->name('plates.index');
+    Route::get('/plates/{plate}', 'PlateController@show')->name('plates.show');
+    Route::get('/plates/create', 'PlateController@create')->name('plates.create');
+    Route::post('/plates', 'PlateController@store')->name('plates.store');
+    Route::get('/plates/edit/{plate}', 'PlateController@edit')->name('plates.edit');
+    Route::patch('/plates/{plate}', 'PlateController@update')->name('plates.update');
+    Route::delete('/plates/{plate}', 'PlateController@destroy')->name('plates.destroy');
+
+    // orders routes
+    Route::get('/orders', 'OrderController@index')->name('orders.index');
+    Route::get('/orders/{plate}', 'OrderController@show')->name('orders.show');
+    // Rotta SoftDelete MEMO
+    // Route::delete('/orders/{plate}', 'OrderController@destroy')->name('orders.destroy');
+
+    // Dovremo inserire in un secondo momento la rotta delle statistiche ordini MEMO
+});
 
 
 
