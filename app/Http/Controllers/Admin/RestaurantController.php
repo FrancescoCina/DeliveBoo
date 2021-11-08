08a84f71282d643dd53bcd85813448bd78e20a3d
DamianoLandi <damianolandi01@gmail.com>
2021-11-08 15:55:03 +0100

    Index Controller

--- app/Http/Controllers/Admin/RestaurantController.php
+++ app/Http/Controllers/Admin/RestaurantController.php
@@ -5,11 +5,19 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Restaurant;
+
+use Illuminate\Support\Facades\Auth;
+
+use App\User;
+
 class RestaurantController extends Controller
 {
     public function index()
     {
-        // $restaurants = $user->restaurant;
+        $newRestaurant = Restaurant::where('user_id', Auth::user()->id)->first();
+
+        return view('admin.restaurants.index', compact('newRestaurant'));
 
     }
 }
