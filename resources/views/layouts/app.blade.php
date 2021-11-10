18aad9c58447f93b176acbea9fc10258fe313732
FrancescoCina <francesco.cina04@gmail.com>
2021-11-10 12:13:06 +0100

    Creato grafico di test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -76,6 +76,8 @@
             @yield('content')
         </main>
     </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script> 
     @yield('script')
 </body>
 </html>
